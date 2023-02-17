c5ce0b0e81f3924395c1dc98a3f7da970f5d5bd5
Utyf <utyff@mail.ru>
2023-02-17 20:17:34 +0300

    ADC 1ch, DMA one shot

--- Src/adc.c
+++ Src/adc.c
@@ -1,9 +1,11 @@
 #include <stm32f3xx.h>
 
+void ADC_init() __attribute__((section (".ccmram")));
+
+void ADC_start() __attribute__((section (".ccmram")));
 
 /**
  * ADC1 channel 1 - PA0
- * ADC2 channel 1 - PA4
  */
 
 
@@ -25,7 +27,7 @@ uint8_t adcResolution = ADC_8BITS;
 // 0b11001: PLL clock divided by 64
 // 0b11010: PLL clock divided by 128
 // 0b11011: PLL clock divided by 256
-uint8_t rccAdcDivider = 0b10001;
+uint8_t rccDivider = 0b10001;
 
 // Delay for interleaved mode. Set only when ADEN=0
 // circle time = SAMPLE_TIME + CONV. TIME = 1.5 + 8.5 = 10 tics
@@ -34,7 +36,7 @@ uint8_t rccAdcDivider = 0b10001;
 // 0b0001 - 2
 // 0b0010 - 3
 // 0b0011 - 4   MAX: 1011 - 12
-uint8_t adcDelay = 0b0010;
+uint8_t interleaveDelay = 0b0010;
 
 //000: 1.5 ADC clock cycles
 //001: 2.5 ADC clock cycles
@@ -46,43 +48,47 @@ uint8_t adcDelay = 0b0010;
 //111: 601.5 ADC clock cycles
 uint8_t sampleTime = 0b000;
 
+// ADC clock
+// 00: (Asynchronous clock mode) PLL
+// 01: HCLK/1 (Synchronous clock mode) AHB
+// 10: HCLK/2
+// 11: HCLK/4
+uint8_t adcClock = 0b10;
+
 #define BUF_SIZE 4096
 uint8_t samplesBuffer[BUF_SIZE];
+uint8_t samplesReady;
+uint8_t samplesError;
 
 void DWT_Delay_us(uint32_t us);
-void DMA_init();
-void ADC_start();
+
+void ADC_init() __attribute__((section (".ccmram")));
+
+void ADC_start() __attribute__((section (".ccmram")));
+
+void DMA_start() __attribute__((section (".ccmram")));
 
 
-void ADC_Init() {
-    DMA_init();
-
+void ADC_init() {
     // init GPIO, VR and calibration run only once
     // Enable clocks GPIOA and GPIOB
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
     // Set mode b11 (analog input) for ADC pins
     GPIOA->MODER |= (0b11u << 0u);      // PA0 for ADC1 ch1
-    GPIOA->MODER |= (0b11u << (4 * 2)); // PA4 for ADC2 ch1
 
     RCC->AHBENR |= RCC_AHBENR_ADC12EN; // turn on ADC12 clock
 
     // Set Prescaler ADC for Asynchronous clock mode
-    MODIFY_REG(RCC->CFGR2, RCC_CFGR2_ADCPRE12_Msk, rccAdcDivider << RCC_CFGR2_ADCPRE12_Pos);
+    MODIFY_REG(RCC->CFGR2, RCC_CFGR2_ADCPRE12_Msk, rccDivider << RCC_CFGR2_ADCPRE12_Pos);
 
     // Set ADC clock
-    // 00: (Asynchronous clock mode) PLL
-    // 01: HCLK/1 (Synchronous clock mode) AHB
-    // 10: HCLK/2
-    // 11: HCLK/4
-    ADC12_COMMON->CCR &= ~ADC_CCR_CKMODE_Msk; // set 00
+    MODIFY_REG(ADC12_COMMON->CCR, ADC_CCR_CKMODE_Msk, adcClock << ADC_CCR_CKMODE_Pos);
 
     // enable the ADC voltage regulator
     ADC1->CR &= ~ADC_CR_ADVREGEN_1;
-    ADC2->CR &= ~ADC_CR_ADVREGEN_1;
 
     ADC1->CR |= ADC_CR_ADVREGEN_0;
-    ADC2->CR |= ADC_CR_ADVREGEN_0;
     DWT_Delay_us(10); // voltage regulator startup time
 
     // start ADC calibration cycle
@@ -90,74 +96,66 @@ void ADC_Init() {
     // wait for calibration to complete
     while (ADC1->CR & ADC_CR_ADCAL);
 
-    // start ADC calibration cycle
-    ADC2->CR |= ADC_CR_ADCAL;
-    // wait for calibration to complete
-    while (ADC2->CR & ADC_CR_ADCAL);
-
-    ADC_start();
+//    ADC_start();
 }
 
 void ADC_start() {
+    DMA_start();
     // Delay for interleaved mode. Set only when ADEN=0
-    ADC12_COMMON->CCR |= (adcDelay << ADC_CCR_DELAY_Pos);
+    ADC12_COMMON->CCR |= (interleaveDelay << ADC_CCR_DELAY_Pos);
 
     // enable the ADC
     ADC1->CR |= ADC_CR_ADEN;
-    ADC2->CR |= ADC_CR_ADEN;
-
     // wait till ADC start
     while (!(ADC1->ISR & ADC_ISR_ADRDY));
-    while (!(ADC2->ISR & ADC_ISR_ADRDY));
 
     // Select ADC Channels
     ADC1->SQR1 = (1u << ADC_SQR1_SQ1_Pos); // 1-st channel for ADC1
-    ADC2->SQR1 = (1u << ADC_SQR1_SQ1_Pos); // 1-st channel for ADC2
-
     // Regular channel sequence length - 1
     ADC1->SQR1 &= ~ADC_SQR1_L_Msk;
-    ADC2->SQR1 &= ~ADC_SQR1_L_Msk;
 
     // Set Prescaler ADC for Asynchronous clock mode
-    MODIFY_REG(RCC->CFGR2, RCC_CFGR2_ADCPRE12_Msk, rccAdcDivider << RCC_CFGR2_ADCPRE12_Pos);
+    MODIFY_REG(RCC->CFGR2, RCC_CFGR2_ADCPRE12_Msk, rccDivider << RCC_CFGR2_ADCPRE12_Pos);
 
     // Set sampling time for channels
     MODIFY_REG(ADC1->SMPR1, ADC_SMPR1_SMP1_Msk, sampleTime << ADC_SMPR1_SMP1_Pos); // ADC1 channel 1
-    MODIFY_REG(ADC2->SMPR1, ADC_SMPR1_SMP1_Msk, sampleTime << ADC_SMPR1_SMP1_Pos); // ADC2 channel 1
 
     // set data resolution
     MODIFY_REG(ADC1->CFGR, ADC_CFGR_RES_Msk, adcResolution << ADC_CFGR_RES_Pos);
-    MODIFY_REG(ADC2->CFGR, ADC_CFGR_RES_Msk, adcResolution << ADC_CFGR_RES_Pos);
 
-    // Enable continuous conversion mode. Only on the master
-    ADC1->CFGR |= ADC_CFGR_CONT; // Master ADC1 + ADC2
+    // Enable continuous conversion mode
+    ADC1->CFGR |= ADC_CFGR_CONT;
 
-    // dual mode - Regular simultaneous mode only
+    // dual mode
     // 00000: Independent mode
     // 00110: Regular simultaneous mode only
     // 00111: Interleaved mode only
-    ADC12_COMMON->CCR |= (0b00111u << ADC_CCR_DUAL_Pos);
+    MODIFY_REG(ADC12_COMMON->CCR, ADC_CCR_DUAL_Msk, 0b00000u << ADC_CCR_DUAL_Pos);
 
     // DMA mode.  0 -> One Shot; 1 -> Circular
     // 0 - stop when DMA_CCR_TCIE
-    ADC12_COMMON->CCR |= ADC_CCR_DMACFG;
+    ADC1->CFGR &= ~ADC_CFGR_DMACFG;
 
-    // COMMON DMA mode b11 - for 8-bit resolution
-    ADC12_COMMON->CCR |= (0b11u << ADC_CCR_MDMA_Pos);
+    // enable DMA mode
+    ADC1->CFGR |= ADC_CFGR_DMAEN;
 
     // ADC start conversion
     ADC1->CR |= ADC_CR_ADSTART;
 
-    __WFI();
+//    __WFI(); // stop CPU
 }
 
-void DMA_init() {
+void DMA_start() {
     // Enable clocks DMA1
     RCC->AHBENR |= RCC_AHBENR_DMA1EN;
 
-    // Transfer complete interrupt enable and Circular mode
-    //DMA1_Channel1->CCR |= DMA_CCR_TCIE | DMA_CCR_HTIE | DMA_CCR_TEIE | DMA_CCR_CIRC;
-    DMA1_Channel1->CCR |= DMA_CCR_CIRC;
+    // Disable DMA
+    DMA1_Channel1->CCR &= ~DMA_CCR_EN;
+
+    // enable interrupts - Transfer complete and error
+    DMA1_Channel1->CCR |= DMA_CCR_TCIE | DMA_CCR_TEIE;
+    // disable Circular mode
+    DMA1_Channel1->CCR &= ~DMA_CCR_CIRC;
 
     // Memory increment mode
     DMA1_Channel1->CCR |= DMA_CCR_MINC;
@@ -166,19 +164,15 @@ void DMA_init() {
     // 00: 8-bits
     // 01: 16-bits
     // 10: 32-bits
-    DMA1_Channel1->CCR |= (0b01u << DMA_CCR_PSIZE_Pos);
-
+    DMA1_Channel1->CCR |= (0b00u << DMA_CCR_PSIZE_Pos);
     // Memory size
-    // 00: 8-bits
-    // 01: 16-bits
-    // 10: 32-bits
-    DMA1_Channel1->CCR |= (0b01u << DMA_CCR_MSIZE_Pos);
+    DMA1_Channel1->CCR |= (0b00u << DMA_CCR_MSIZE_Pos);
 
     // Channel Priority level 11 - Very high
     DMA1_Channel1->CCR |= DMA_CCR_PL_Msk;
 
-    // Number of data to transfer. 2 samples in 1 transfer.
-    DMA1_Channel1->CNDTR = BUF_SIZE / 2;
+    // Number of data to transfer
+    DMA1_Channel1->CNDTR = BUF_SIZE;
 
     // Peripheral address register
     DMA1_Channel1->CPAR = (uint32_t) &ADC12_COMMON->CDR;
@@ -186,37 +180,39 @@ void DMA_init() {
     // Memory address register
     DMA1_Channel1->CMAR = (uint32_t) (&samplesBuffer);
 
-    // Reset interrupt flags
-    DMA1->IFCR |= DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1;
+    // clear interrupt flags
+    DMA1->IFCR = DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1;
 
     // Enable DMA
     DMA1_Channel1->CCR |= DMA_CCR_EN;
 
-//    NVIC_EnableIRQ(DMA1_Channel1_IRQn); // enable DMA1 CH1 interrupt
+    NVIC_EnableIRQ(DMA1_Channel1_IRQn); // enable DMA1 CH1 interrupt
 }
 
-uint16_t dmaG = 0;
 uint16_t dmaT = 0;
-uint16_t dmaH = 0;
 uint16_t dmaE = 0;
-uint32_t adcCircleStart;
-uint32_t adcCircleTicks;
 
 void DMA1_Channel1_IRQHandler() {
     if (DMA1->ISR & DMA_ISR_TCIF1) { // transfer complete
+        // ADC stop conversion
+        ADC1->CR |= ADC_CR_ADSTP;
+        samplesReady = 1;
+
         dmaT++;
-        uint32_t ticks = DWT->CYCCNT;
-        adcCircleTicks = ticks - adcCircleStart;
-        adcCircleStart = ticks;
-    }
-    if (DMA1->ISR & DMA_ISR_HTIF1) { // half transfer
-        dmaH++;
+        // toggle PB0
+        uint32_t odr = GPIOB->ODR;
+        // Set selected pins that were at low level, and reset ones that were high
+        GPIOB->BSRR = ((odr & GPIO_ODR_0) << 16U) | (~odr & GPIO_ODR_0);
+        DMA1->IFCR = DMA_IFCR_CTCIF1; // clear interrupt flag
     }
     if (DMA1->ISR & DMA_ISR_TEIF1) {
+        DMA1->IFCR = DMA_IFCR_CTEIF1;
         dmaE++;
+        samplesError = 1;
+        // toggle PB1
+        uint32_t odr = GPIOB->ODR;
+        // Set selected pins that were at low level, and reset ones that were high
+        GPIOB->BSRR = ((odr & GPIO_ODR_1) << 16U) | (~odr & GPIO_ODR_1);
     }
-    if (DMA1->ISR & DMA_ISR_GIF1) {
-        dmaG++;
-    }
-    DMA1->IFCR |= DMA_IFCR_CGIF1;
+    DMA1->IFCR = DMA_IFCR_CGIF1;
 }
--- Src/main.c
+++ Src/main.c
@@ -1,12 +1,20 @@
 #include <stm32f3xx.h>
 
-void ADC_Init();
 
 #define SYSTEM_CORE_CLOCK 72000000
 #define DWT_IN_MICROSEC (SYSTEM_CORE_CLOCK/1000000)
 
+extern uint8_t samplesReady;
+extern uint8_t samplesError;
 
-void DWT_Init() {
+void ADC_init() __attribute__((section (".ccmram")));
+
+void ADC_start() __attribute__((section (".ccmram")));
+
+int main(void) __attribute__((section (".ccmram")));
+
+
+void DWT_init() {
     if (!(CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk)) {
         CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
         DWT->CYCCNT = 0;
@@ -23,13 +31,13 @@ void DWT_Delay_us(uint32_t us) {
 }
 
 void SystemClock_Config(void) {
-    // External oscillator (HSE) = 24MHz
+    // External oscillator (HSE) = 8MHz
     // SYS_CLK = 72MHz
     // APB1 = 36MHz
     RCC->CR |= RCC_CR_HSEON;           // Enable HSE
     while ((RCC->CR & RCC_CR_HSERDY) == 0);
 
-    RCC->CFGR |= RCC_CFGR_PLLMUL3;     // PLL MUL = x3, SYS_CLK=72MHz (x9 for 8MHz)
+    RCC->CFGR |= RCC_CFGR_PLLMUL9;     // PLL MUL = x9, SYS_CLK=72MHz (x3 for 24MHz)
     RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;  // APB1 Prescaler = 2
     RCC->CFGR |= RCC_CFGR_PLLSRC;      // PLL source = HSE
 
@@ -43,9 +51,23 @@ void SystemClock_Config(void) {
 int main(void) {
     SystemClock_Config();
 
-    DWT_Init();
-    ADC_Init();
+    DWT_init();
+    ADC_init();
 
+    // GPIO init
+    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+    // control bits PB0 PB1 OUT mode & very high speed
+    GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR0_Msk | GPIO_OSPEEDER_OSPEEDR1_Msk;
+    MODIFY_REG(GPIOB->MODER, (GPIO_MODER_MODER0_Msk | GPIO_MODER_MODER0_Msk), (GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0));
+
+    ADC_start();
+
+#pragma ide diagnostic ignored "EndlessLoop"
     while (1) {
+        if (samplesReady | samplesError) {
+            samplesReady = 0;
+            samplesError = 0;
+            ADC_start();
+        }
     }
 }
